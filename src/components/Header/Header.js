0be21e774d0d29827610b05adddb58d9fa6939b4
fuckence <sancho.serik2003s.s@gmail.com>
2024-05-28 13:32:58 +0500

    ddd

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React,{ useState } from 'react'
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import './Header.css'
 import { Link as ScrollLink, animateScroll as scroll  } from 'react-scroll';
 
