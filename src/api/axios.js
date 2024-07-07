ea83705cb1fb3454f002c4681e7d8743b1f28eb2
fuckence <sancho.serik2003s.s@gmail.com>
2024-07-08 01:26:24 +0500

    BaseUrl changed

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'http://77.243.81.210:8080/api',
     validateStatus: () => true,
 });
 
